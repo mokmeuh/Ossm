48c25dfccb246894eac96f0a39769699fca00e05
Mikael Langlois <mikael.langlois@gmail.com>
2026-07-03 21:20:18 -0400

    chore: retire app/build, .gradle, .kotlin du suivi git (artefacts de build)

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_ossm_remote_OssmApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.ossm.remote.OssmApplication"
-)
-public final class _com_ossm_remote_OssmApplication {
-}
